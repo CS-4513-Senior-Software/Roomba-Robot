1d603bf981c081d399654940ce5e19f0365e5079
Caleb Tietz <calebmtietz@gmail.com>
2025-02-27 15:10:33 -0600

    updated main

--- hardware/raspberry_pi/main.py
+++ hardware/raspberry_pi/main.py
@@ -5,7 +5,7 @@ import math
 import socket
 import struct
 import serial
-import pygame
+# import pygame
 
 
 SPEED = [1, 5, 15, 25, 50, 99]
@@ -76,12 +76,12 @@ try:
         
         # pygame.event.pump()
         
-        axis_values = [1, 1, 0, 0]
+        axis_values = []
         # for i in range(4):
         #     axis_value = joystick.get_axis(i)
         #     axis_values.append(axis_value)
         
-        button_values = 1
+        button_values = 4096
         # for i in range(12):
         #     button_value = joystick.get_button(i)  # Returns 0 or 1
         #     button_values = (button_values << 1) | button_value
@@ -96,18 +96,18 @@ try:
             tilt = tilt_default
             pan = pan_default
         
-        # if(button_values & 64):
-        #     speed_mode = 5
-        # if(button_values & 128):
-        #     speed_mode = 4
-        # if(button_values & 256):
-        #     speed_mode = 3             
-        # if(button_values & 512):
-        #     speed_mode = 2
-        # if(button_values & 1024):
-        #     speed_mode = 1
-        # if(button_values & 2048):
-        #     speed_mode = 0                
+        if(button_values & 64):
+            speed_mode = 5
+        if(button_values & 128):
+            speed_mode = 4
+        if(button_values & 256):
+            speed_mode = 3             
+        if(button_values & 512):
+            speed_mode = 2
+        if(button_values & 1024):
+            speed_mode = 1
+        if(button_values & 2048):
+            speed_mode = 0                
         
         if(abs(axis_values[AXIS_PAN]) > axis_dead):
             pan = pan - 0.8*axis_values[AXIS_PAN]
