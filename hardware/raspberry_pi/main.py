d600f2e46bd4f3fbd1d52480d15950e413e177b2
Caleb Tietz <calebmtietz@gmail.com>
2025-03-04 14:41:04 -0600

    fixed the stopping issue

--- hardware/raspberry_pi/main.py
+++ hardware/raspberry_pi/main.py
@@ -11,6 +11,7 @@ import typing
 class DigitalWriteException(Exception):
     pass
 
+prev_integers = [450, 450, 0, 0]
 SPEED = [1, 2, 3, 4, 5, 6]
 speed_mode = 0
 mc_inn = [0,0,0,0]
@@ -22,29 +23,130 @@ AXIS_PAN = 2
 AXIS_FB = 1
 AXIS_LR = 0
 
-def digital_write(currInts: list[int], endInts: list[int], bool_byte, easing = True, n_steps = 100):
-        if len(currInts) != len(endInts) or len(currInts) != 4:
-            raise DigitalWriteException("current and ending lists must both be of length 4")
+def digital_write(axis_values: list[int], easing = True, n_steps = 15):
+    global prev_integers
+    if len(axis_values) != 4:
+        raise DigitalWriteException("Length of axis_values array must be equal to 4.")
 
-        if (not easing):
-            data = struct.pack('>BIIIIB',0xFF, *endInts, bool_byte)
+    data = get_integers_bool(axis_values)
+    endInts = data["ints"]
+    bool_byte = data["bool_byte"]
+    
+    if (not easing):
+        data = struct.pack('>BIIIIB',0xFF, *endInts, bool_byte)
+        ser.write(data)
+        return
+    
+    
+    stepSize0 = math.ceil((endInts[0] - prev_integers[0]) / n_steps)
+    stepSize1 = math.ceil((endInts[1] - prev_integers[1]) / n_steps)
+    stepSize2 = math.ceil((endInts[2] - prev_integers[2]) / n_steps)
+    stepSize3 = math.ceil((endInts[3] - prev_integers[3]) / n_steps)
+    
+    stepSizes = [stepSize0, stepSize1, stepSize2, stepSize3]
+    
+    for i in range(n_steps):
+        for j, k in enumerate(prev_integers):
+            stepSize = stepSizes[j]
+            prev_integers[j] += stepSize
+            data = struct.pack('>BIIIIB',0xFF, *prev_integers, bool_byte)
             ser.write(data)
-            return
+    
+    # copy endInts into prev_integers
+    prev_integers = []
+    for i in endInts:
+        prev_integers.append(i)
+
+def get_integers_bool(axis_values: list[int]):
+    global pan
+    global tilt
+    global speed_mode
+    
+    button_values = 0
+    # for i in range(12):
+    #     button_value = joystick.get_button(i)  # Returns 0 or 1
+    #     button_values = (button_values << 1) | button_value
+    #     #button_values.append(button_value)
         
-        stepSize0 = (endInts[0] - currInts[0]) / n_steps
-        stepSize1 = (endInts[1] - currInts[1]) / n_steps
-        stepSize2 = (endInts[2] - currInts[2]) / n_steps
-        stepSize3 = (endInts[3] - currInts[3]) / n_steps
+    print(f"axis values: {axis_values}, button values: {button_values}")
         
-        stepSizes = [stepSize0, stepSize1, stepSize2, stepSize3]
+    #servos
+    if(button_values & 1):
+        tilt = tilt_default
+        pan = pan_default
+    
+    if(button_values & 64):
+        speed_mode = 5
+    if(button_values & 128):
+        speed_mode = 4
+    if(button_values & 256):
+        speed_mode = 3             
+    if(button_values & 512):
+        speed_mode = 2
+    if(button_values & 1024):
+        speed_mode = 1
+    if(button_values & 2048):
+        speed_mode = 0                
+    
+    if(abs(axis_values[AXIS_PAN]) > axis_dead):
+        pan = pan - 0.8*axis_values[AXIS_PAN]
+        if(pan > 179):
+            pan = 179
+        if(pan < 1):
+            pan = 1
+    if(abs(axis_values[AXIS_TILT]) > axis_dead):
+        tilt = tilt - 0.8*axis_values[AXIS_TILT]
+        if(tilt > 179):
+            tilt = 179
+        if(tilt < 1):
+            tilt = 1
+    
+    if(abs(axis_values[AXIS_TILT]) > axis_dead):
+        tilt = tilt - 0.8*axis_values[AXIS_TILT]
+        if(tilt > 179):
+            tilt = 179
+        if(tilt < 1):
+            tilt = 1
+            
         
-        for i in range(n_steps):
-            for j, k in enumerate(currInts):
-                stepSize = stepSizes[j]
-                currInts[j] += stepSize
-                data = struct.pack('>BIIIIB',0xFF, *currInts, bool_byte)
-                ser.write(data)
-           
+        
+    # Motors
+    L_motor = 0
+    R_motor = 0
+    
+    L_motor = axis_values[AXIS_FB] - axis_values[AXIS_LR]
+    R_motor = axis_values[AXIS_FB] + axis_values[AXIS_LR]
+    L_motor = clamp(L_motor, -1, 1)
+    R_motor = clamp(R_motor, -1, 1)
+    print("L_motor " + str(L_motor))
+    print("R_motor " + str(R_motor))
+
+    if(abs(L_motor) > 0.01 or abs(R_motor) > 0.01):
+        if(L_motor <= 0 and R_motor <= 0): # Reverse
+            mc_inn = [1, 0, 1, 0]
+        elif(L_motor <= 0 and R_motor > 0):
+            mc_inn = [0, 1, 1, 0]
+        elif(L_motor > 0 and R_motor <= 0):
+            mc_inn = [1, 0, 0, 1]
+        elif(L_motor > 0 and R_motor > 0): #Forward
+            mc_inn = [0, 1, 0, 1]
+        pwm_l = int(((abs(L_motor) * 127.5) + 127.5))
+        pwm_r = int(((abs(R_motor) * 127.5) + 127.5))
+    else:
+        pwm_l = 0
+        pwm_r = 0
+        mc_inn = [0, 0, 0, 0]
+
+    pan_i = int(map_range(pan, 1, 179, 150, 600))
+    tilt_i = int(map_range(tilt, 1, 179, 150, 600))
+    integers = [pan_i, tilt_i, pwm_l, pwm_r]
+    bool_byte = (mc_inn[0] << 0) | (mc_inn[1] << 1) | (mc_inn[2] << 2) | (mc_inn[3] << 3)
+    
+    return {
+        "ints": integers,
+        "bool_byte": bool_byte
+    }
+  
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
 def clamp(value, min_value, max_value):
@@ -86,114 +188,38 @@ pygame.init()
 # print(f"Joystick connected: {joystick.get_name()}")
 
 
-axis_values = [0, 0, 0, 0]
-prev_integers = [0, 0, 0, 0]
+
 try:
-    while True:
         
-        pygame.event.pump()
+    pygame.event.pump()
+    
+    # axis_values = []
+    # for i in range(4):
+    #     axis_value = joystick.get_axis(i)
+    #     axis_values.append(axis_value)
+    
+    values = [
+        [0, -0.5, 0, 0],
+        [0, -0.5, 0, 0],
+        [0, -0.5, 0, 0],
+        [0, 0.5, 0, 0],
+        [0, 0.5, 0, 0],
+        [0, 0.5, 0, 0],
+        [0.5, 0, 0, 0]
+    ]
+    
+    for val in values:
+        digital_write(val)
         
-        # axis_values = []
-        # for i in range(4):
-        #     axis_value = joystick.get_axis(i)
-        #     axis_values.append(axis_value)
-        
-        button_values = 0
-        # for i in range(12):
-        #     button_value = joystick.get_button(i)  # Returns 0 or 1
-        #     button_values = (button_values << 1) | button_value
-        #     #button_values.append(button_value)
-            
-        print(f"axis values: {axis_values}, button values: {button_values}")
-            
-        #servos
-        if(button_values & 1):
-            tilt = tilt_default
-            pan = pan_default
-        
-        if(button_values & 64):
-            speed_mode = 5
-        if(button_values & 128):
-            speed_mode = 4
-        if(button_values & 256):
-            speed_mode = 3             
-        if(button_values & 512):
-            speed_mode = 2
-        if(button_values & 1024):
-            speed_mode = 1
-        if(button_values & 2048):
-            speed_mode = 0                
-        
-        if(abs(axis_values[AXIS_PAN]) > axis_dead):
-            pan = pan - 0.8*axis_values[AXIS_PAN]
-            if(pan > 179):
-                pan = 179
-            if(pan < 1):
-                pan = 1
-        if(abs(axis_values[AXIS_TILT]) > axis_dead):
-            tilt = tilt - 0.8*axis_values[AXIS_TILT]
-            if(tilt > 179):
-                tilt = 179
-            if(tilt < 1):
-                tilt = 1
-                
-            
-            
-        # Motors
-        L_motor = 0
-        R_motor = 0
-        
-        #if(abs(axis_values[1]) > axis_dead or axis_values[0] > axis_dead):
-        L_motor = -(axis_values[AXIS_FB] - axis_values[AXIS_LR])
-        if(abs(axis_values[AXIS_TILT]) > axis_dead):
-            tilt = tilt - 0.8*axis_values[AXIS_TILT]
-            if(tilt > 179):
-                tilt = 179
-            if(tilt < 1):
-                tilt = 1
-                
-            
-            
-        # Motors
-        L_motor = 0
-        R_motor = 0
-        
-        #if(abs(axis_values[1]) > axis_dead or axis_values[0] > axis_dead):
-        L_motor = -(axis_values[AXIS_FB] - axis_values[AXIS_LR])
-        R_motor = -(axis_values[AXIS_FB] + axis_values[AXIS_LR])
-        #print(f"L_motor: {L_motor}  R_motor: {R_motor}")
-        # Clamp motor outputs to -1 to 1
-        L_motor = clamp(L_motor, -1, 1)
-        R_motor = clamp(R_motor, -1, 1)
-        print("L_motor " + str(L_motor))
-        print("R_motor " + str(R_motor))
-
-        if(abs(L_motor) > 0.01 or abs(R_motor) > 0.01):
-            if(L_motor <= 0 and R_motor <= 0): # Reverse
-                mc_inn = [1, 0, 1, 0]
-            elif(L_motor <= 0 and R_motor > 0):
-                mc_inn = [0, 1, 1, 0]
-            elif(L_motor > 0 and R_motor <= 0):
-                mc_inn = [1, 0, 0, 1]
-            elif(L_motor > 0 and R_motor > 0): #Forward
-                mc_inn = [0, 1, 0, 1]
-            pwm_l = int(((abs(L_motor) * 127.5) + 127.5)/SPEED[speed_mode])
-            pwm_r = int(((abs(R_motor) * 127.5) + 127.5)/SPEED[speed_mode])
-        else:
-            pwm_l = 0
-            pwm_r = 0
-            mc_inn = [0, 0, 0, 0]
-
-        pan_i = int(map_range(pan, 1, 179, 150, 600))
-        tilt_i = int(map_range(tilt, 1, 179, 150, 600))
-        integers = [pan_i, tilt_i, pwm_l, pwm_r]
-        bool_byte = (mc_inn[0] << 0) | (mc_inn[1] << 1) | (mc_inn[2] << 2) | (mc_inn[3] << 3)
-        digital_write(prev_integers, integers, bool_byte, 100)
-        prev_integers = integers
-
-
-        # if (axis_values[0] + 0.01 < 1):
-        #     axis_values[0] += 0.01
+    
+    # axis_values = [0, -1, 0, 0]
+    # print(prev_integers)
+    # digital_write(axis_values)
+    # print(prev_integers)
+    # time.sleep(2)
+    # axis_values = [0, 0.1, 0, 0]
+    # digital_write(axis_values)
+    
 except KeyboardInterrupt:
     print("Exiting...")
 finally:
